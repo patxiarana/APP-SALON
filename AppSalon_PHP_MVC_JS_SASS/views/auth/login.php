2de77ab9aa02327a5eb93b4cca0a6bca6c00d4ef
patxi <patxiarana05@gmail.com>
2024-02-05 14:28:07 -0300

    crendo el formulario de autenticacion

--- AppSalon_PHP_MVC_JS_SASS/views/auth/login.php
+++ AppSalon_PHP_MVC_JS_SASS/views/auth/login.php
@@ -1,2 +1,28 @@
 <h1 class="nombre-pagina">Login</h1>
 <p class="descripcion-pagina">Inicia sesion con tus datos</p>
+
+<form class="formulario" method="POST" action="/">
+    <div class="campo">
+        <label for="email">Email </label>
+        <input type="email"
+        id="email"
+        placeholder="Tu email..."
+        name="email"
+         />
+    </div>
+    <div class="campo">
+        <label for='password'>Password</label>
+        <input type="password"
+         id="password"
+         placeholder="Tu Password..."
+         name="password"
+         />
+    </div>
+
+    <input type="submit" class="boton" value="Iniciar Sesión">
+</form>
+
+<div class="acciones">
+    <a href="/crear-cuenta">Aun no tiens una cuenta ? Crear una</a>
+    <a href="/olvide">Olvidaste tu password?</a>
+</div>
