03d7d902d9f7aa6a55620d7721f85abda6827421
unknown <patxiarana05@gmail.com>
2024-06-12 10:05:30 -0300

    mostrar mensaje de exito o error

--- AppSalon_PHP_MVC_JS_SASS/controllers/LoginController.php
+++ AppSalon_PHP_MVC_JS_SASS/controllers/LoginController.php
@@ -75,7 +75,22 @@ class LoginController {
       $alertas = [] ; 
 
       $token = s($_GET['token']);
+
       $usuario = Usuario::where('token', $token); 
+
+     if(empty($usuario)) {
+      //mostrar mensaje de error 
+      Usuario::setAlerta('error', 'Token no valido') ; 
+     } else {
+      //modificar a usuario confirmado 
+   $usuario->confirmado = "1" ; 
+   $usuario->token = null ; 
+   $usuario->guardar() ; 
+   Usuario::setAlerta('exito', 'Cuenta Comprobada Correctamente') ; 
+     }
+//obtener alertas 
+     $alertas = Usuario::getAlertas(); 
+ //renderizar la vista 
       $router->render('auth/confirmar-cuenta',[
           'alertas'=> $alertas 
         ]) ; 
--- AppSalon_PHP_MVC_JS_SASS/views/auth/confirmar-cuenta.php
+++ AppSalon_PHP_MVC_JS_SASS/views/auth/confirmar-cuenta.php
@@ -2,3 +2,8 @@
 
 
 <?php include_once __DIR__ . "../../templates/alertas.php" ; ?>
+
+
+<div class="acciones">
+    <a href="/">Iniciar Sesion</a>
+</div>
