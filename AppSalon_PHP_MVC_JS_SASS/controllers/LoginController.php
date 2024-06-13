d5d50613ab644f03a07fc7604aebbeb1d473c986
unknown <patxiarana05@gmail.com>
2024-06-13 17:38:19 -0300

    validando al usuario

--- AppSalon_PHP_MVC_JS_SASS/controllers/LoginController.php
+++ AppSalon_PHP_MVC_JS_SASS/controllers/LoginController.php
@@ -22,7 +22,24 @@ class LoginController
        
       if($usuario) {
         //Verificar el password
-      $usuario->comprobarPasswordAndVerificado($auth->password) ; 
+    if($usuario->comprobarPasswordAndVerificado($auth->password)) {
+      //Autenticar al usuario 
+      session_start();
+      $_SESSION['id'] = $usuario->id;
+      $_SESSION['nombre'] = $usuario->nombre . " " . $usuario->apellido;
+      $_SESSION['email'] = $usuario->email;
+      $_SESSION['login'] = true;
+
+       //Redireccionamiento 
+       if($usuario->admin === 1) {
+        $_SESSION['admin'] = $usuario->admin ?? null;
+        header('Location: /admin');
+       } else {
+        header('Location: /cita');
+       } ; 
+
+
+    } ; 
 
       } else {
         Usuario::setAlerta('error', 'Usuario no encontrado') ; 
