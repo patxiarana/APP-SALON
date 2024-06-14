6fbd3a435c7acd8f6ebe1e49b81b216e7c6e739c
unknown <patxiarana05@gmail.com>
2024-06-14 13:12:01 -0300

    validando el token

--- AppSalon_PHP_MVC_JS_SASS/views/auth/recuperar-password.php
+++ AppSalon_PHP_MVC_JS_SASS/views/auth/recuperar-password.php
@@ -1,22 +1,26 @@
 <h1 class="nombre-pagina">Recuperar Password</h1>
-<p class="descripcion-pagina">Coloca tu nuevo password a continuacion</p>
+<p class="descripcion-pagina">Coloca tu nuevo password a continuación</p>
 
+<?php 
+    include_once __DIR__ . "/../templates/alertas.php";
+?>
 
+<?php if($error) return; ?>
 <form class="formulario" method="POST">
-
-<div class="campo">
-    <label for="password">Password</label>
-    <input type="password"
-    id="password"
-    name="password"
-    placeholder="Tu password..."
-    />
-</div>
-<input type="submit" class="boton" value="Guardar Nuevo Password">
+    <div class="campo">
+        <label for="password">Password</label>
+        <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Tu Nuevo Password"
+        />
+    </div>
+    <input type="submit" class="boton" value="Guardar Nuevo Password">
 
 </form>
 
 <div class="acciones">
-    <a href="/">Ya tienes cuenta ? Iniciar Sesion</a>
-    <a href="/crear-cuenta">Aun no tienes cuenta ? Obtener una</a>
+    <a href="/">¿Ya tienes cuenta? Iniciar Sesión</a>
+    <a href="/crear-cuenta">¿Aún no tienes cuenta? Obtener una</a>
 </div>
