b9d82f334f9b58937ec07134a2aee8d5971b88dd
unknown <patxiarana05@gmail.com>
2024-06-15 09:01:08 -0300

    creando el routing para citas

--- AppSalon_PHP_MVC_JS_SASS/public/index.php
+++ AppSalon_PHP_MVC_JS_SASS/public/index.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\CitaController;
 use Controllers\LoginController;
 use MVC\Router;
 
@@ -25,5 +26,10 @@ $router->post('/crear-cuenta', [LoginController::class, 'crear']) ;
 //Confirmar cuenta 
 $router->get('/confirmar-cuenta', [LoginController::class, 'confirmar']) ; 
 $router->get('/mensaje', [LoginController::class, 'mensaje']) ; 
+
+//Area privada 
+$router->get('/cita', [CitaController::class, 'index']) ; 
+
+
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
