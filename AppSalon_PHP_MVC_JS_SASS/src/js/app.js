c209358cb212cf3e90eb5b561bc72b2438e02b15
unknown <patxiarana05@gmail.com>
2024-06-16 11:25:52 -0300

    creando el paginador

--- AppSalon_PHP_MVC_JS_SASS/src/js/app.js
+++ AppSalon_PHP_MVC_JS_SASS/src/js/app.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
 function iniciarApp() {
     mostrarSeccion()// Muestra y oculta las secciones 
     tabs(); // Cambia la seccciones cuando se precionen los tabs 
+    botonesPaginador() ; //Agrega o quita los botones del paginador 
 }
 function mostrarSeccion() {
 //Ocultar la seccion que tenga la clase de mostrar 
@@ -39,6 +40,23 @@ function tabs() {
      paso = parseInt(e.target.dataset.paso)
 
      mostrarSeccion()
+     botonesPaginador() ;
     }) ; 
     });
 }
+
+function botonesPaginador() {
+    const paginaAnterior = document.querySelector('#anterior') ;
+    const paginaSiguiente = document.querySelector('#siguiente') ; 
+    
+    if(paso === 1) {
+        paginaAnterior.classList.add('ocultar');
+        paginaSiguiente.classList.remove('ocultar'); 
+    } else if (paso ===3) {
+        paginaAnterior.classList.remove('ocultar'); 
+        paginaSiguiente.classList.add('ocultar');
+    } else {
+        paginaAnterior.classList.remove('ocultar'); 
+        paginaSiguiente.classList.remove('ocultar');
+    }
+}    
