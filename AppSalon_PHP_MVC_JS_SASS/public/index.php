1c944bae5a94b098c1cc330ae9d67ba405a90248
unknown <patxiarana05@gmail.com>
2024-06-17 10:54:45 -0300

    creando el modelo de servicios y exportando a json

--- AppSalon_PHP_MVC_JS_SASS/public/index.php
+++ AppSalon_PHP_MVC_JS_SASS/public/index.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\ApiController;
 use Controllers\CitaController;
 use Controllers\LoginController;
 use MVC\Router;
@@ -29,6 +30,9 @@ $router->get('/mensaje', [LoginController::class, 'mensaje']) ;
 
 //Area privada 
 $router->get('/cita', [CitaController::class, 'index']) ; 
+
+//Api de citas 
+$router->get('/api/servicios', [ApiController::class, 'index']) ; 
 
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
