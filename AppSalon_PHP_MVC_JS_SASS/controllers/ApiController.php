793786c5faad6b1501db6f9daa63282ae7ab5b4e
unknown <patxiarana05@gmail.com>
2024-06-21 10:07:04 -0300

    almacenando las citas con postman

--- AppSalon_PHP_MVC_JS_SASS/controllers/ApiController.php
+++ AppSalon_PHP_MVC_JS_SASS/controllers/ApiController.php
@@ -2,6 +2,7 @@
 
 namespace Controllers;
 
+use Model\Cita;
 use Model\Servicio;
 
 class ApiController {
@@ -11,10 +12,9 @@ class ApiController {
     }
 
     public static function guardar() {
-        $respuesta = [
-            'datos'=> $_POST,
-         
-        ] ; 
-        echo json_encode($respuesta);
+        $cita = new Cita($_POST) ; 
+
+        $resultado = $cita->guardar(); 
+        echo json_encode($resultado);
     }
 }
