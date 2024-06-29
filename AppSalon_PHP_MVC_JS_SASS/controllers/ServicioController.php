3fdeb04711a2f441d957983ba71c17b8477213c8
unknown <patxiarana05@gmail.com>
2024-06-29 10:40:46 -0300

    creando las vistas para los servicios

--- AppSalon_PHP_MVC_JS_SASS/controllers/ServicioController.php
+++ AppSalon_PHP_MVC_JS_SASS/controllers/ServicioController.php
@@ -6,15 +6,26 @@ use MVC\Router ;
 
 class ServicioController {
     public static function index(Router $router) {
-       $router->render('servicios/index') ; 
+        error_reporting(E_ALL & ~E_NOTICE);
+        ini_set('display_errors', '1');
+        session_start() ;
+
+       $router->render('servicios/index', [
+         'nombre' => $_SESSION['nombre'],
+       ]) ; 
     }
 
     public static function crear(Router $router) {
-     
+        error_reporting(E_ALL & ~E_NOTICE);
+        ini_set('display_errors', '1');
+        session_start() ;
         
         if($_SERVER['REQUEST_METHOD']==='POST') {
 
         }
+        $router->render('servicios/crear', [
+            'nombre' => $_SESSION['nombre'],
+          ]) ; 
     }
 
 
@@ -24,6 +35,10 @@ class ServicioController {
         if($_SERVER['REQUEST_METHOD']==='POST') {
             
         }
+
+        $router->render('servicios/actualizar', [
+            'nombre' => $_SESSION['nombre'],
+          ]) ; 
     }
 
 
---
+++ AppSalon_PHP_MVC_JS_SASS/views/servicios/crear.php
@@ -0,0 +1,9 @@
+<h1 class="nombre-pagina">Nuevo Serivicio</h1>
+<p class="descripcion-pagina">Llena todos los campos para añadir un nuevo servicio</p>
+
+
+<?php  
+
+include_once __DIR__ . '/../templates/barra.php'
+
+?>
