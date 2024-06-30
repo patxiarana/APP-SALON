959cfeb6ccec4f5a39449126b792801ede4b67c5
unknown <patxiarana05@gmail.com>
2024-06-30 11:49:02 -0300

    css a los servicios

--- AppSalon_PHP_MVC_JS_SASS/views/servicios/index.php
+++ AppSalon_PHP_MVC_JS_SASS/views/servicios/index.php
@@ -14,6 +14,18 @@ include_once __DIR__ . '/../templates/barra.php'
     <li>
         <p>Nombre: <span><?php echo $servicio->nombre ; ?></span></p>
         <p>Precio: <span>$<?php echo $servicio->precio ; ?></span></p>
+
+        <div class="acciones">
+            <a class="boton" href="/servicios/actualizar?id=<?php echo $servicio->id ;?>">Actualizar</a>
+
+            <form action="/servicios/eliminar" method="POST">
+                <input type="hidden"
+                name="id"
+                value="<?php echo $servicio->id; ?>"
+                >
+                <input type="submit" value="Borrar" class="boton-eliminar">
+            </form>
+        </div>
     </li>
 
     <?php }?>
